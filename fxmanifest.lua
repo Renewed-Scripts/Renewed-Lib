48ff86e6541f1e8974af19c3a47b9cab23c1e5f4
FjamZoo <laurids112@live.dk>
2024-01-17 08:49:21 +0100

    1.1.5
    - reworked stashshop to be more performant and easier to read

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 
 author "Renewed Scripts | FjamZoo#0001"
 description 'Renewed Library for a bundle of functions / scripts for servers to use.'
-version '1.1.4'
+version '1.1.5'
 
 shared_script {
     '@ox_lib/init.lua',
--- server/stashshop.lua
+++ server/stashshop.lua
@@ -6,8 +6,6 @@ local inventories = {}
 local openedBy = {}
 local shops = {}
 
-local inventoryHook
-
 local function addItemToSecondSlot(id, price, payload)
     SetTimeout(50, function()
         local addMoney = ox_inventory:AddItem(id, 'money', price, nil, 1)
@@ -39,10 +37,6 @@ local function prepStash(id, label, coords)
     ox_inventory:ClearInventory(id, false)
 end
 
-local function deleteHook()
-    ox_inventory:removeHooks(inventoryHook)
-    inventoryHook = nil
-end
 
 local function createSaleStash(id, label, items, coords)
     id = ('stashshop_%s'):format(id)
@@ -56,80 +50,76 @@ local function createSaleStash(id, label, items, coords)
     inventories[id] = 2
     shops[id] = items
 
-    if inventoryHook then
-        deleteHook()
+end exports('CreateSaleStash', createSaleStash)
+
+
+ox_inventory:registerHook('swapItems', function(payload)
+    local source = payload.source
+    local item = payload.fromSlot.name:lower()
+    local addItem = inventories[payload.toInventory]
+    local inventory = payload.toInventory == source and payload.fromInventory or payload.toInventory
+
+    if payload.toInventory == payload.fromInventory then
+        return false
     end
 
-    inventoryHook = ox_inventory:registerHook('swapItems', function(payload)
-        local source = payload.source
-        local item = payload.fromSlot.name:lower()
-        local addItem = inventories[payload.toInventory]
-        local inventory = payload.toInventory == source and payload.fromInventory or payload.toInventory
-
-        if payload.toInventory == payload.fromInventory then
-            return
-        end
-
-        if item == 'money' and not addItem then
-            if payload.count < payload.fromSlot.count then
-                return false
-            end
-
-            resetInventory(source, inventory)
-            return true
-        end
-
-        if payload.action ~= 'move' then
+    if item == 'money' and not addItem then
+        if payload.count < payload.fromSlot.count then
             return false
         end
 
-        local price = shops?[inventory]?[item]
-        if not price or openedBy[inventory] ~= source then
+        resetInventory(source, inventory)
+        return true
+    end
+
+    if payload.action ~= 'move' then
+        return false
+    end
+
+    local price = shops?[inventory]?[item]
+    if not price or openedBy[inventory] ~= source then
+        return false
+    end
+
+    price *= payload.count
+
+    local slotCount = inventories[inventory]
+
+    if addItem then
+        if payload.toSlot == 1 then
+            addItemToSecondSlot(inventory, price, payload)
             return false
-        end
+        else
+            local added = ox_inventory:AddItem(inventory, 'money', price, nil, 1)
 
-        price *= payload.count
-
-        local slotCount = inventories[inventory]
-
-        if addItem then
-            if payload.toSlot == 1 then
-                addItemToSecondSlot(inventory, price, payload)
-
-                return false
-            else
-                local added = ox_inventory:AddItem(inventory, 'money', price, nil, 1)
-
-                if added then
-                    slotCount += 1
-                    ox_inventory:SetSlotCount(inventory, slotCount)
-
-                    inventories[inventory] = slotCount
-                end
-
-                return added
-            end
-        elseif payload.fromSlot.slot > 1 and payload.fromSlot.slot == slotCount-1 then
-            local removed = ox_inventory:RemoveItem(inventory, 'money', price, nil, 1)
-
-            if removed then
-                slotCount -= 1
+            if added then
+                slotCount += 1
                 ox_inventory:SetSlotCount(inventory, slotCount)
 
                 inventories[inventory] = slotCount
             end
 
-            return removed
+            return added
+        end
+    elseif payload.fromSlot.slot > 1 and payload.fromSlot.slot == slotCount-1 then
+        local removed = ox_inventory:RemoveItem(inventory, 'money', price, nil, 1)
+
+        if removed then
+            slotCount -= 1
+            ox_inventory:SetSlotCount(inventory, slotCount)
+
+            inventories[inventory] = slotCount
         end
 
-        return false
-    end, {
-        inventoryFilter = {
-            '^stashshop_[%w]+'
-        }
-    })
-end exports('CreateSaleStash', createSaleStash)
+        return removed
+    end
 
+    return false
+end, {
+    inventoryFilter = {
+        '^stashshop_[%w]+'
+    }
+})
 
 AddEventHandler('ox_inventory:openedInventory', function(playerId, inventoryId)
     if inventories[inventoryId] and not openedBy[inventoryId] then
