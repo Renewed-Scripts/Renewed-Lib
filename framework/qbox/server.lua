41578047da8a40001018bfd25fd8a66b1eb1b2de
Zoo <laurids112@live.dk>
2024-08-28 17:32:38 -0400

    feat: finish qbox framework module

--- framework/qbox/server.lua
+++ framework/qbox/server.lua
@@ -4,142 +4,97 @@ end
 
 local Controller = require 'framework.server'
 
-
-
-
-
-
-
-local Players, Jobs, Gangs = {}, {}, {}
-
-function Renewed.getGroups(src)
-    return Players[src] and Players[src].Groups or false
-end
-
-function Renewed.hasGroup(src, group, grade)
-    local Player = Players[src]
-
-    if not Player then return false end
-    if not Player.Groups[group] then return false end
-
-    if grade then return Player.Groups[group] >= grade end
-
-    return true
-end
-
-function Renewed.getPlayer(source)
-    return Players[source]
-end
-
-function Renewed.addStress(source, value)
+---Adds stress to a player by their source
+---@param source number
+---@param value number
+exports('addStress', function(source, value)
     local Player = exports.qbx_core:GetPlayer(source)
     local stress = (Player.PlayerData.metadata.stress or 0) + value
 
-    Player.Functions.SetMetaData('stress', stress > 100 and 100 or stress)
-    exports.qbx_core:Notify(source, "Stress Gained", 'error', 1500)
-end
+    Player.Functions.SetMetaData('stress', lib.math.clamp(stress, 0, 100))
+    exports.qbx_core:Notify(source, 'Stress Gained', 'error', 1500)
+end)
 
-function Renewed.relieveStress(source, value)
+---Relieves stress from a player by their source
+---@param source number
+---@param value number
+exports('relieveStress', function(source, value)
     local Player = exports.qbx_core:GetPlayer(source)
     local stress = (Player.PlayerData.metadata.stress or 0) - value
 
-    Player.Functions.SetMetaData('stress', stress < 0 and 0 or stress)
-    exports.qbx_core:Notify(source, "Stress Relieved", 'error', 1500)
-end
+    Player.Functions.SetMetaData('stress', lib.math.clamp(stress, 0, 100))
+    exports.qbx_core:Notify(source, 'Stress Relieved', 'error', 1500)
+end)
 
+---Returns the amount of money a player has by their source
+---@param source number
+---@param moneyType 'cash' | 'bank'
+---@return number?
+exports('getMoney', function(source, moneyType)
+    local Player = exports.qbx_core:GetPlayer(source)
 
-function Renewed.isGroupAuth(group, grade)
-    grade = tostring(grade)
-    local numGrade = tonumber(grade)
-    local Group = Jobs[group] or Gangs[group]
-    local auth = false
-    if Group then
-        auth = Group.grades[grade] and Group.grades[grade].isboss or Group.grades[numGrade] and Group.grades[numGrade].isboss
+    if Player then
+        return Player.PlayerData.money[moneyType]
     end
-    return auth
-end
+end)
 
-function Renewed.getCharId(src)
-    return Players[src] and Players[src].charId or false
-end
-
-function Renewed.getCharName(src)
-    return Players[src] and Players[src].name or false
-end
-
-local query = 'SELECT charinfo FROM players WHERE citizenid = ?'
-function Renewed.getCharNameById(identifier)
-    local charinfo = MySQL.prepare.await(query, {identifier})
-    if not charinfo then return false end
-    charinfo = json.decode(charinfo)
-    local fullname = ("%s %s"):format(charinfo.firstname, charinfo.lastname)
-    return fullname
-end
-
-function getMoney(src, mType)
-    local Player = exports.qbx_core:GetPlayer(src)
-    if not Player then return end
-    return Player.PlayerData.money[mType]
-end
-
-local GET_OFFLINEMONEY = 'SELECT money FROM players WHERE citizenid = ?'
-function getOfflineMoney(id)
-    local result = MySQL.query.await(GET_OFFLINEMONEY, {id})
-    if not result then return false end
-    return {bank = result.bank, cash = result.cash}
-end
-
-function removeMoney(src, amount, mType, reason)
+---Removes money from a player's account by their source
+---@param src number
+---@param amount number
+---@param moneyType 'cash' | 'bank'
+---@param reason string?
+---@return boolean
+exports('removeMoney', function(src, amount, moneyType, reason)
     local Player = exports.qbx_core:GetPlayer(src)
 
-    if not Player then return end
+    if Player and Player.PlayerData.money[moneyType] >= amount then
+        return Player.Functions.RemoveMoney(moneyType, amount, reason or "unknown")
+    end
 
-    if Player.PlayerData.money[mType] < amount then return end
+    return false
+end)
 
-    return Player.Functions.RemoveMoney(mType, amount, reason or "unknown")
-end
+---Adds money to a player's account by their source
+---@param source number
+---@param amount number
+---@param moneyType 'cash' | 'bank'
+---@param reason string?
+---@return boolean
+exports('addMoney', function(source, amount, moneyType, reason)
+    local Player = exports.qbx_core:GetPlayer(source)
 
-function addMoney(src, amount, mType, reason)
-    local Player = exports.qbx_core:GetPlayer(src)
+    if Player then
+        return Player.Functions.AddMoney(moneyType, amount, reason or "unknown")
+    end
 
-    if not Player then return end
+    return false
+end)
 
-    return Player.Functions.AddMoney(mType, amount, reason or "unknown")
-end
+---Adds needs (hunger, thirst) to a player, returns true/false depending on success and reason why it failed
+---@param source number
+---@param needs { hunger: number, thirst: number }
+---@return boolean
+---@return string?
+exports('addNeeds', function(source, needs)
+    if type(needs) ~= 'table' then return false, 'NEEDS IS NOT A TABLE' end
 
-local ADD_OFFLINEMONEY = "UPDATE players SET money = JSON_SET(money, CONCAT('$.', ?), JSON_UNQUOTE(JSON_EXTRACT(money, CONCAT('$.', ?))) + ?) WHERE citizenid = ?"
-function addOfflineMoney(identifier, amount, mType)
-    return MySQL.prepare.await(ADD_OFFLINEMONEY, {mType, mType, amount, identifier})
-end
+    local Player = exports.qbx_core:GetPlayer(source)
 
-local REMOVE_OFFLINEMONEY = "UPDATE players SET money = JSON_SET(money, CONCAT('$.', ?), JSON_UNQUOTE(JSON_EXTRACT(money, CONCAT('$.', ?))) - ?) WHERE citizenid = ?"
-function removeOfflineMoney(identifier, amount, mType)
-    return MySQL.prepare.await(REMOVE_OFFLINEMONEY, {mType, mType, amount, identifier})
-end
+    if not Player then return false, 'PLAYER NOT FOUND' end
 
-function addNeeds(src, needs)
-    if type(needs) ~= "table" then return end
-
-    local Player = exports.qbx_core:GetPlayer(src)
-
-    if not Player then return end
-
-    needs.hunger = needs.hunger or 0
-    needs.thirst = needs.thirst or 0
-
-    local hunger = Player.PlayerData.metadata["hunger"] + needs.hunger
-    local thirst = Player.PlayerData.metadata["thirst"] + needs.thirst
+    local hunger = Player.PlayerData.metadata.hunger + (needs.hunger or 0)
+    local thirst = Player.PlayerData.metadata.thirst + (needs.thirst or 0)
 
     if hunger > 100 then hunger = 100 end
     if thirst > 100 then thirst = 100 end
 
-    Player.Functions.SetMetaData("hunger", hunger)
-    Player.Functions.SetMetaData("thirst", thirst)
+    Player.Functions.SetMetaData('hunger', lib.math.clamp(hunger, 0, 100))
+    Player.Functions.SetMetaData('thirst', lib.math.clamp(thirst, 0, 100))
 
-    TriggerClientEvent('hud:client:UpdateNeeds', src, hunger, thirst)
+    TriggerClientEvent('hud:client:UpdateNeeds', source, hunger, thirst)
 
     return true
-end
+end)
 
 
 AddEventHandler('qbx_core:server:onGroupUpdate', function(source, groupName, groupGrade)
@@ -162,7 +117,7 @@ AddEventHandler('QBCore:Server:PlayerLoaded', function(Player)
     end
 
     Controller.createPlayer(Player.PlayerData.source, {
-        name = ('%s %s'):format(Player.PlayerData.charinfo.firstname, Player.PlayerData.charinfo.lastname),
+        name = Player.PlayerData.charinfo.firstname .. ' ' .. Player.PlayerData.charinfo.lastname,
         charId = Player.PlayerData.citizenid,
         Groups = groups,
     })
@@ -174,3 +129,6 @@ AddEventHandler('QBCore:Server:OnPlayerUnload', Controller.removePlayer)
 AddEventHandler('playerDropped', function()
     Controller.removePlayer(source)
 end)
+
+
+require 'framework.qbox.db'
--- framework/server.lua
+++ framework/server.lua
@@ -6,9 +6,6 @@ local framework = require 'classes.framework'
 local Players = {}
 
 
-
-
-
 ---Gets a player object by their source
 ---@param source number
 ---@return framework_class?
@@ -44,6 +41,32 @@ function Controller.getSourceByCharId(charId)
         end
     end
 end exports('getSourceByCharId', Controller.getSourceByCharId)
+
+
+---Checks if a player is in a group and if great is provided, checks if they are that grade or higher
+---@param source number
+---@param group string
+---@param grade number?
+---@return boolean
+function Controller.hasGroup(source, group, grade)
+    local Player = Players[source]
+
+    if not Player or not Player.Groups then return false end
+
+    if grade then
+        return Player.Groups[group] >= grade
+    end
+
+    return true
+end exports('hasGroup', Controller.hasGroup)
+
+---Gets a player's groups
+---@param source number
+---@return table<string, number> | false
+function Controller.getGroups(source)
+    return Players[source] and Players[source].Groups or false
+end exports('getGroups', Controller.getGroups)
+
 
 
 ---Creates a player object and assigns it to the class
