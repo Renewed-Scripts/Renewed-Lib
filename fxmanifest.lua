0f4be9f4c5347775cf803a26d0158e7612fee1c6
Zoo <laurids112@live.dk>
2024-08-28 18:38:41 -0400

    Feat: add manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+
+author "Renewed Scripts"
+description 'Renewed Scripts is a collection of functions and classes to help you create more compatability between your scripts.'
+version '2.0.0'
+
+shared_script {
+    '@ox_lib/init.lua',
+    'framework/init.lua'
+}
+
+client_scripts {
+    'modules/**/client.lua',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'modules/**/server.lua',
+}
+
+
+files {
+    'init.lua',
+    'classes/*.lua',
+	'framework/**/client.lua',
+}
+
+dependency 'ox_lib'
