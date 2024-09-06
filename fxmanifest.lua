4db7f7c1f502bcc6a3d47bd45943eb00abebe795
solareon <solareon@users.noreply.github.com>
2024-09-06 19:47:31 +0000

    chore: bump manifest version to v2.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 
 author "Renewed Scripts"
 description 'Renewed Scripts is a collection of functions and classes to help you create more compatability between your scripts.'
-version '2.0.0'
+version '2.0.1'
 
 shared_script {
     '@ox_lib/init.lua',
